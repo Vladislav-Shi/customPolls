b7cd88e524dc9adb986bbebbdb67ab46f34354b7
Vladislav-Shi <vlad.shirobockow@yandex.ru>
2024-02-21 13:35:36 +0300

    fix

--- base/settings.py
+++ base/settings.py
@@ -140,20 +140,3 @@ REST_FRAMEWORK = {
     ),
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
---
+++ docker/frontend_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd app
+ls -al
+pwd
+npm run serve
